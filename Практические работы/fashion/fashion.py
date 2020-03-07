eabe3601df1c5a1da0cfc684e230a1108ed0dbb7
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-07 22:17:20 +0300

    fashion: Добавлена возможность загружать свои изображения

--- Практические работы/fashion/fashion.py
+++ Практические работы/fashion/fashion.py
@@ -52,3 +52,29 @@ print('np.argmax(predictions[0])', np.argmax(predictions[0]))
 print('np.argmax(y_train[0])', np.argmax(y_train[0]))
 print('scores[1]: ', model.evaluate(x_test, y_test, verbose=1)[1])
 
+import glob
+for file in glob.glob("*.jpg"):
+    print(file)
+img_path = input('image path: ')
+img = image.load_img(img_path, target_size=(28, 28), color_mode = "grayscale")
+
+# Преобразуем картинку в массив
+x = image.img_to_array(img)
+# Меняем форму массива в плоский вектор
+x = x.reshape(1, 784)
+# Инвертируем изображение
+x = 255 - x
+# Нормализуем изображение
+x /= 255
+
+"""Запускаем распознавание"""
+
+prediction = model.predict(x)
+
+"""Результаты распознавания"""
+
+print(prediction)
+
+prediction = np.argmax(prediction)
+print("Номер класса:", prediction)
+print("Название класса:", classes[prediction])
