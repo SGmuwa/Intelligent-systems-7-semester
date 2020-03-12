4f538c466df7f52dc40933ff5c96aca8f21596f6
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-12 15:43:51 +0300

    Рекуррентная: Попытаться восстановить работу

--- Лабораторные работы/recurrent_neural_network.py
+++ Лабораторные работы/recurrent_neural_network.py
@@ -12,10 +12,14 @@ class HopfieldNetwork:
 
     def _calculate_pseudoinverse_matrix(self, F):
         '''Вычисление псевдоинверсной матрицы'''
-        F_t = F.values()
+        F_t = F.transpose()
         M_inv = np.linalg.inv((np.dot(F_t, F)))
         F_plus = np.dot(M_inv, F_t)
         return F_plus
+
+    def _reshape(self, data):
+        data = np.array(data).transpose()
+        return data
 
     def signum_func(self, net):
         if net > 0:
@@ -26,6 +30,7 @@ class HopfieldNetwork:
             return 0  # означает, что нужно сохранить предыдущее значение выхода
 
     def fit(self, train_data):
+        train_data = self._reshape(train_data)
         train_data_plus = self._calculate_pseudoinverse_matrix(train_data)
         self.weights = np.dot(train_data, train_data_plus)
         np.fill_diagonal(self.weights, 0)
@@ -55,119 +60,126 @@ class HopfieldNetwork:
         for el in test_data:
             self._pred(el)
 
+    def _print_(self, t, data):
+        print(str(t)+"  ")
+        for i in range(0, data.size, self.w_input):
+            l = data[i:i+self.w_input]
+            l = [" " if j == -1 else "@" for j in l]
+            print(l)
+        print("\n")
 
-answers = {
-    0:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+
+# ⬜⬛
+answers = [
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜",
-    1:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬛⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬛⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜",
-    2:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜",
-    4:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
+
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜",
-    5:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜",
-    6:
-    "⬜⬜⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+
+    "⬜⬜⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜"
-}
+]
 
-tests = {
-    1:
-    "⬛⬜⬜⬜⬜⬜⬜" +
-    "⬛⬛⬛⬛⬛⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬛⬜⬜" +
-    "⬜⬜⬛⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬜⬜⬜" +
-    "⬜⬜⬜⬛⬛⬜⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+tests = [
+    #1:
+    "⬛⬜⬜⬜⬜⬜⬜\n" +
+    "⬛⬛⬛⬛⬛⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬛⬜⬜\n" +
+    "⬜⬜⬛⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬜⬜⬜\n" +
+    "⬜⬜⬜⬛⬛⬜⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬛",
-    0:
-    "⬛⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬛⬛⬜⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬜⬛⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬛" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
+    #0:
+    "⬛⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬛⬛⬜⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬜⬛⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬛\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
     "⬜⬜⬜⬛⬜⬜⬜",
-    4:
-    "⬜⬛⬜⬜⬜⬜⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬜⬜⬜⬛⬜" +
-    "⬜⬛⬛⬜⬛⬛⬜" +
-    "⬜⬛⬛⬛⬛⬛⬜" +
-    "⬜⬜⬜⬛⬜⬛⬜" +
-    "⬜⬜⬛⬜⬜⬛⬜" +
-    "⬜⬜⬜⬜⬜⬛⬜" +
+    #4:
+    "⬜⬛⬜⬜⬜⬜⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬜⬜⬜⬛⬜\n" +
+    "⬜⬛⬛⬜⬛⬛⬜\n" +
+    "⬜⬛⬛⬛⬛⬛⬜\n" +
+    "⬜⬜⬜⬛⬜⬛⬜\n" +
+    "⬜⬜⬛⬜⬜⬛⬜\n" +
+    "⬜⬜⬜⬜⬜⬛⬜\n" +
     "⬜⬜⬜⬜⬜⬜⬜"
-}
-
+]
 
 def convertStringToIntArray(string):
-    return np.array([(1 if s == '⬛' else -1) for s in string])
+    return [(1 if s == '⬛' else -1) for s in string]
 
 # Конвектирует string в Array<int>.
 
 
 def convertInputFromStringToIntArrays(inputDataDictionary):
-    outputData = {}
-    for key, string in inputDataDictionary.items():
-        outputData[key] = convertStringToIntArray(string)
+    outputData = [None] * len(inputDataDictionary)
+    for key, string in enumerate(inputDataDictionary):
+        outputData[key] = convertStringToIntArray(string.replace('\n', ''))
     return outputData
 
-
-answers = convertInputFromStringToIntArrays(answers)
-tests = convertInputFromStringToIntArrays(tests)
+answers_num = convertInputFromStringToIntArrays(answers)
+tests_num = convertInputFromStringToIntArrays(tests)
 
 model = HopfieldNetwork(w_input=7, h_input=9)
-model.fit(answers)
+model.fit(answers_num)
+print(tests[model.prediction(tests_num[0])])
