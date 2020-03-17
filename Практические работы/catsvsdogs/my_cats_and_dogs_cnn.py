063f8a25720c16d08135924a0d77a4194e3cb2af
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-17 19:04:28 +0300

    Добавить import GaussianNoise

--- Практические работы/catsvsdogs/my_cats_and_dogs_cnn.py
+++ Практические работы/catsvsdogs/my_cats_and_dogs_cnn.py
@@ -3,7 +3,7 @@
 
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense, GaussianNoise
 from tensorflow.python.keras.datasets import cifar10
 from tensorflow.keras.models import load_model
 from tensorflow.keras import utils
