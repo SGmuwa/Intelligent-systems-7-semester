ce0ddc59dae3f5ce60809f4af1233324933968ea
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-17 14:06:01 +0300

    my_cats_and_dogs_cnn: Реализовать загрузку начальных данных cifar10

--- Практические работы/catsvsdogs/my_cats_and_dogs_cnn.py
+++ Практические работы/catsvsdogs/my_cats_and_dogs_cnn.py
@@ -13,15 +13,11 @@ from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
 from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.python.keras.datasets import cifar10
 
-# Каталог с данными для обучения
-train_dir = 'train'
-# Каталог с данными для проверки
-val_dir = 'val'
-# Каталог с данными для тестирования
-test_dir = 'test'
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 # Размеры изображения
-img_width, img_height = 150, 150
+img_width, img_height = 32, 32
 # Размерность тензора на основе изображения для входных данных в нейронную сеть
 # backend Tensorflow, channels_last
 input_shape = (img_width, img_height, 3)
@@ -30,11 +26,11 @@ epochs = 30
 # Размер мини-выборки
 batch_size = 16
 # Количество изображений для обучения
-nb_train_samples = 17500
-# Количество изображений для проверки
-nb_validation_samples = 3750
+nb_train_samples = 50000
+# Количество изображений для проверки в процентах
+nb_validation_samples = 0.2
 # Количество изображений для тестирования
-nb_test_samples = 3750
+nb_test_samples = 10000
 
  
 # # Создаем генератор изображений
