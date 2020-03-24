503196942ae182bfceac1c9d57e2d3666e97ff1f
SG_Muwa <motherlode.muwa@gmail.com>
2020-03-24 03:04:01 +0300

    Курсовая: Сделать загрузку более большого количества данных

--- Курсовая работа/learn.py
+++ Курсовая работа/learn.py
@@ -22,6 +22,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.layers import Dense
 from download_data import getFileIterator
+import numpy as np
 
 model = Sequential()
 model.add(Dense(1000, input_dim=980, activation="sigmoid"))
@@ -32,7 +33,31 @@ model.compile(
     metrics = ["accuracy"],
     loss = mean_absolute_error)
 
-#for path in getFileIterator():
-if True:
-    for (x, y) in generator_bad_and_good_sound('/tmp/vk_db/Feel It Still (Zhu Remix)   Portugal. The Man.mp3'):
-        model.fit(x, y, epochs=10, validation_split=0.0, verbose=1)
+default_array = np.zeros((980), dtype=float)
+X = None
+Y = None
+for path in getFileIterator():
+    for (x, y) in generator_bad_and_good_sound(path):
+        if len(x[0]) != 980:
+            target_size = np.zeros((980))
+            target_size[:x[0].shape[0]] = x[0]
+            x = np.array([target_size])
+        if len(y[0]) != 1000:
+            target_size = np.zeros((1000))
+            target_size[:y[0].shape[0]] = y[0]
+            y = np.array([target_size])
+        if X is None:
+            X = x
+        else:
+            X = np.concatenate((X, x))
+        if Y is None:
+            Y = y
+        else:
+            Y = np.concatenate((Y, y))
+        print('\r{:.2%} '.format(len(X) / 100000.), end='', flush=True)
+        if len(X) >= 100000:
+            print()
+            model.fit(X, Y, epochs=100, validation_split=0.1, verbose=1)
+            X = None
+            Y = None
+    print()
